21ca94f98c101e76eef669187928e11892fda825
Ryan Oblak <rroblak@gmail.com>
2013-11-12 09:25:22 -0800

    Initial import.

---
+++ src/org/openhrv/ChooseYourDeviceActivity.java
@@ -0,0 +1,23 @@
+package org.openhrv;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ChooseYourDeviceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_your_device);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.choose_your_device, menu);
+        return true;
+    }
+    
+}
